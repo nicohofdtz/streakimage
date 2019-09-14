9b041bb4dcc1403e6d16d6773c448c62973d3e9d
Nico Hofeditz <nicohofeditz@gmail.com>
2019-09-15 00:58:18 +0200

    Parsing header

--- load_file.py
+++ load_file.py
@@ -1,15 +1,4 @@
 from streak_image import StreakImage
 
-test = StreakImage(
-    "test/test_file_180613 000 TRPL 60x %dDN216Co_TBP1%d%lL446nm%l%uOC%u%i0uA%i 10x5s_28mW_446nm_OD01,50_mono620_ST4.img")
-test.height = "string"
-print(test.height)
-
-
-def f(t):
-    print(x)
-
-
-x = 1
-f(x)
-print(x)
+test = StreakImage("test/test_file.img")
+# print(test.height)
--- streak_image.py
+++ streak_image.py
@@ -2,6 +2,8 @@ import datetime
 from datetime import datetime
 from enum import Enum
 
+from  import parse
+
 
 class FileType(Enum):
     """Enum for the file types used by HPD-TA
@@ -15,18 +17,78 @@ class FileType(Enum):
 
 
 class StreakImage:
-    """Parses and holds the data of an recorded image"""
+    """Parses and holds the data of an recorded image
+    
+        args:
+            file_path: Path to the file to be parsed
 
-    def __init__(self, file_path):
-        self.date: datetime = None
-        self.comment_length: int = None
-        self.width: int = None
-        self.height: int = None
-        self.x_offset: int = None
-        self.y_offset: int = None
-        self.file_type: FileType = None
+    """
+
+    def __init__(self, file_path: str):
+        self.date: datetime
+        self.comment_length: int
+        self.width: int
+        self.height: int
+        self.x_offset: int
+        self.y_offset: int
+        self.file_type: FileType
+        self.data: list
 
         self.parse_file(file_path)
+        # print("Date:", self.date)
+        print("Comment length:", self.comment_length)
+        print("Width:", self.width, "Height:", self.height)
+        print("x-offset:", self.x_offset, "y-offset", self.y_offset)
+        print("file-type:", self.file_type.name)
 
-    def parse_file(self, file_path):
-        file = open(file_path, "rb")
+    def parse_file(self, file_path: str):
+        """Read the given file and parse the content to class fields.
+        
+        args:
+            file_path: Path to the file to be parsed
+
+        """
+        with open(file_path, "rb") as file:
+            file_content = file.read()
+            self.im_chars = file_content[0:2].decode("utf-8")
+            self.comment_length = int.from_bytes(
+                file_content[2:4], byteorder="little", signed=False
+            )
+            self.width = int.from_bytes(
+                file_content[4:6], byteorder="little", signed=False
+            )
+            self.height = int.from_bytes(
+                file_content[6:8], byteorder="little", signed=False
+            )
+            self.x_offset = int.from_bytes(
+                file_content[8:9], byteorder="little", signed=False
+            )
+            self.y_offset = int.from_bytes(
+                file_content[10:11], byteorder="little", signed=False
+            )
+            file_type_int = int.from_bytes(
+                file_content[12:13], byteorder="little", signed=False
+            )
+            self.file_type = FileType(file_type_int)
+            nnn = 64 + self.comment_length
+            self.comment = file_content[64 : nnn]
+            self.data = self.parse_data(file_content[nnn:])
+    
+    def parse_data(self, binary_data: bytes):
+        """Parse given data bytes to 2d list
+        
+        args:
+            binary_data: The part of the file containing the intensity data.
+        
+        """
+        data = []
+        byte_per_pixel = 2 if self.file_type == FileType.BIT8 else 4
+        
+        # for x in range(0, self.width):
+        #     column = []
+        #     for y in range(0, self.height):
+        #         column
+        #     data[i]
+
+
+        return data
