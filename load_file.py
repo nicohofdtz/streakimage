521fa490cebbe8c8eb5c11f7dd2852f42a2cfd52
Nico Hofeditz <nico.hofeditz@physik.uni-marburg.de>
2019-09-18 11:25:50 +0200

    added verbose  mode

--- load_file.py
+++ load_file.py
@@ -1,4 +1,13 @@
 from streak_image import StreakImage
+import argparse
 
-test = StreakImage("test/test_file2.img")
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-v", "--verbose", action="store_true", default=False, help="Verbose mode."
+)
+
+args = parser.parse_args()
+verbose = args.verbose
+
+test = StreakImage("test/test_file2.img", verbose)
 # print(test.height)
