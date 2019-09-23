2ffb8bd05ef78296f8433544fc5c6f6d3f480b26
Nico (Uni) <Nico.Hofeditz@physik.uni-marburg.de>
2019-09-23 13:21:27 +0200

    begin fix errors in comment parse

--- streak_image.py
+++ streak_image.py
@@ -140,6 +140,8 @@ class StreakImage:
             category_name, category_body = re.match(
                 r"\[(.*?)\]\,(.*)", category
             ).groups()
+            print(category_name)
+            print(category_body)
 
             key_rex = r"[a-zA-Z0-9 ]*"
             value_rex = r"[a-zA-Z0-9 ]*"
@@ -150,7 +152,7 @@ class StreakImage:
             category_dict = dict(re.findall(key_val_pair_rex, category_body))
 
             comment_dict[category_name] = category_dict
-
+            print(comment_dict)
         if self.verbose:
             print("Comment parsed. This is the resulting dict:")
             for val in comment_dict:
@@ -158,7 +160,8 @@ class StreakImage:
                 print(val + ":")
                 print("\n" + "-" * 30)
                 for entry in comment_dict[val]:
-                    print("\t{:_<22s}:{:s}".format(entry, comment_dict[val][entry]))
+                    print("\t{:_<22s}:{:s}".format(
+                        entry, comment_dict[val][entry]))
                 print("\n" + "-" * 30)
             print("-" * 60 + "\n")
         return comment_dict
@@ -168,7 +171,8 @@ class StreakImage:
 
         if self.height != other.height:
             raise IndexError(
-                "Height differs: {:s} vs {:s}".format(self.height, other.height)
+                "Height differs: {:s} vs {:s}".format(
+                    self.height, other.height)
             )
 
         if self.width != other.width:
