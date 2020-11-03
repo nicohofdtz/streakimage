d6bdc7562a2bfa520631c08fb7f18122ef9f2e71
Nico Hofeditz <nicohofeditz@gmail.com>
2020-11-03 09:24:33 +0100

    changes name to lowercase

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
 
 setup(
-    name="StreakImage",
-    version="1.0",
+    name="streakimage",
+    version="1.1",
     description="Python Class for ITEX-Files",
     author="nicohfdtz",
     url="https://github.com/nicohofdtz/streakimage",
