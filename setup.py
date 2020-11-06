39507b7f413992dc7d66f2f61fa50ec3a2782fef
Nico Hofeditz <nicohofeditz@gmail.com>
2020-11-06 12:36:57 +0100

    FIX: Including package data Patch

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup
 
 setup(
     name="streakimage",
-    version="2.0",
+    version="2.1",
     description="Python Class for streak camera files.",
     author="Nico Hofeditz",
     author_email="nicohfdtz@gmail.com",
     url="https//github.com/nicohfdtz/streakimage",
     packages=["streakimage"],
+    package_data={"streakimage": ["correction_data/*"]},
 )
