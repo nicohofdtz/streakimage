7c11fa053eb4075e8641b4ee007cc121cd7e4694
Nico Hofeditz <nicohofeditz@gmail.com>
2020-11-12 13:15:10 +0100

    Add config file to package data
    Minor version bump
    Introduce patch version

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 
 setup(
     name="streakimage",
-    version="2.1",
+    version="2.2",
     description="Python Class for streak camera files.",
     author="Nico Hofeditz",
     author_email="nicohfdtz@gmail.com",
     url="https//github.com/nicohfdtz/streakimage",
     packages=["streakimage"],
-    package_data={"streakimage": ["correction_data/*"]},
+    package_data={"streakimage": ["correction_data/*", "streakimage.ini"]},
 )
