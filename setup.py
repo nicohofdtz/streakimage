292b5db9ff70e8aad124a8b65c826590ad7732aa
Nico Hofeditz <nicohofeditz@gmail.com>
2020-11-12 13:17:07 +0100

    Introduce patch level

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="streakimage",
-    version="2.2",
+    version="2.2.0",
     description="Python Class for streak camera files.",
     author="Nico Hofeditz",
     author_email="nicohfdtz@gmail.com",
