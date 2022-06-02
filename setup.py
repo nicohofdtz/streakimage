568ee5e9cf0c3014c7a2ea5f9c63adfbaf0daaea
Nico Hofeditz <nicohofeditz@gmail.com>
2022-06-02 14:48:49 +0200

    Fix some package_data not included

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
 # from setuptools import find_packages
 from setuptools import setup
+from pathlib import Path
+
+streak_data = [
+    str(path.relative_to("streakimage"))
+    for path in Path("streakimage/correction_data").rglob("*.npy")
+]
+streak_data.append("streakimage.ini")
 
 setup(
     name="streakimage",
-    version="2.3.5",
+    version="2.3.6",
     description="Python Class for streak camera files.",
     author="Nico Hofeditz",
     author_email="nicohfdtz@gmail.com",
     url="https//github.com/nicohfdtz/streakimage",
     packages=["streakimage"],
-    package_data={"streakimage": ["correction_data/*", "streakimage.ini"]},
+    package_data={"streakimage": streak_data},
 )
